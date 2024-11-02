15ab64119aaea15dc66f4f1a4deb354719fb4e85
sanmijingbrahma <bsanmijing@gmail.com>
2024-11-02 18:24:41 +0530

    Set up a route to get specific flight informations based on it's id

--- backend/routes/flight.js
+++ backend/routes/flight.js
@@ -8,6 +8,11 @@ router.get('/',(req,res)=>{
     res.json({message:"Fetching all the Flights"});
 })
 
+router.get("/:id",(req,res)=>{
+    const flightID = req.params.id;
+    res.json({message:`fetching data related to ${flightID}`})
+})
+
 router.post('/',(req,res)=>{
     res.json({message:"Adding a new flight"});
 })
